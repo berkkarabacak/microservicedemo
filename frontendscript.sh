d4588df5d20f83ccacffaf598a3341140d69a8e5
berkkarabacak <berk.karabacak@thermofisher.com>
2019-05-31 20:03:21 +0300

    bugfix

--- frontendscript.sh
+++ frontendscript.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export APP_PORT="1113"
-export STATIC_URL="http://$2:8000"
+export STATIC_URL="http://$1:8000"
 export QUOTE_SERVICE_URL="http://$2:1111"
 export NEWSFEED_SERVICE_URL="http://$3:1112"
 export NEWSFEED_SERVICE_TOKEN='T1&eWbYXNWG1w1^YGKDPxAWJ@^et^&kX'
