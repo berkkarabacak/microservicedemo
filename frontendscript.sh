f247a37604ce8bd06d37eadde3ecb6f162def66c
berkkarabacak <berk.karabacak@thermofisher.com>
2019-05-31 16:16:03 +0300

    http added

--- frontendscript.sh
+++ frontendscript.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export APP_PORT="1113"
-export STATIC_URL="$2:8000"
-export QUOTE_SERVICE_URL="$2:1111"
-export NEWSFEED_SERVICE_URL="$3:1112"
+export STATIC_URL="http://$2:8000"
+export QUOTE_SERVICE_URL="http://$2:1111"
+export NEWSFEED_SERVICE_URL="http://$3:1112"
 export NEWSFEED_SERVICE_TOKEN='T1&eWbYXNWG1w1^YGKDPxAWJ@^et^&kX'
 sudo apt-get install leiningen -y
 sudo apt-get install build-essential -y
